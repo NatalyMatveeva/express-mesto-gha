4a2da488b4890e28bf6c06ffd72b4ca09b4675b4
Ivan Matveev <matveeviv@ya.ru>
2022-09-15 22:08:10 +0300

    Проектная работа 13 Версия 2

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,7 +18,7 @@ const createCard = (req, res) => {
 };
 
 const deleteCard = (req, res) => {
-  Card.deleteById(req.params.cardId)
+  Card.findByIdAndDelete(req.params.cardId)
     .then(
       (card) => {
         if (!card) return res.status(404).send({ message: 'Такой карточки не существует' });
